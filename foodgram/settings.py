c656f3feca8275e2df8fd6034d41b2c22cbde25d
Boris Sokolov <tjsonyk@MacBook-Pro-Boris.local>
2021-03-11 01:23:37 +0300

    Localization for Russian DONE. Сделана форма регистрации

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -21,6 +21,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
+    'users',
     'recipes',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -96,7 +97,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
