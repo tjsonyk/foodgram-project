b83ed10969b324131c335bac2158eae6954c71a7
Boris Sokolov <tjsonyk@MacBook-Pro-Boris.local>
2021-04-05 23:19:02 +0300

    Tag filters DONE

--- recipes/templatetags/recipe_form_filters.py
+++ recipes/templatetags/recipe_form_filters.py
@@ -1,7 +1,26 @@
 from django import template
+from django.http import QueryDict
+
+from recipes.models import Recipe, Tag
 
 register = template.Library()
 
 @register.filter
 def addclass(field, css):
     return field.as_widget(attrs={"class": css})
+
+@register.filter(name='get_filter_values')
+def get_filter_values(value):
+    return value.getlist('filters')
+
+@register.filter(name='get_filter_link')
+def get_filter_link(request, tag):
+    new_request = request.GET.copy()
+    if tag.value in request.GET.getlist('filters'):
+        filters = new_request.getlist('filters')
+        filters.remove(tag.value)
+        new_request.setlist('filters', filters)
+    else:
+        new_request.appendlist('filters', tag.value)
+    
+    return new_request.urlencode()
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404
 from rest_framework import filters, mixins, serializers, viewsets
 from django_filters import rest_framework as rest_filters, NumberFilter, CharFilter
 import django_filters
+from django.core.paginator import Paginator
 
 from .models import Recipe, User, Ingredient, Tag, Dimension, Amount
 from .forms import RecipeForm
@@ -13,8 +13,18 @@ from .helpers import get_ingredients
 
 def index(request):
     title = 'Рецепты'
+    tags_values = request.GET.getlist('filters')
     recipe_list = Recipe.objects.all()
-    return render(request, 'indexNotAuth.html', {'title': title, 'recipe_list': recipe_list})
+    tags = Tag.objects.all()
+
+    if tags_values:
+        recipe_list = recipe_list.filter(
+            tag__value__in=tags_values).distinct().all()
+
+    paginator = Paginator(recipe_list, 3)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    return render(request, 'indexNotAuth.html', {'title': title, 'page': page, 'tags': tags})
 
 @login_required
 def new_recipe(request):
