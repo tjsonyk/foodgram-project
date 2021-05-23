1df322d181e1b7869f0025f852e7d102aacaaf28
Boris Sokolov <tjsonyk@gmail.com>
2021-05-23 23:16:43 +0300

    bug fix

--- recipes/filters.py
+++ recipes/filters.py
@@ -1,5 +1,7 @@
 from django_filters import rest_framework as rest_filters, CharFilter
 
+from .models import Ingredient
+
 
 class IngredientFilter(rest_filters.FilterSet):
     query = CharFilter(field_name='title', lookup_expr='icontains')
