1dd9e37f341525c272bb60bcf19078906bc872f8
Boris Sokolov <tjsonyk@gmail.com>
2021-05-23 23:35:01 +0300

    filters.py bugfix

--- recipes/filters.py
+++ recipes/filters.py
@@ -2,7 +2,7 @@ from django_filters import rest_framework as rest_filters, CharFilter
 
 from .models import Ingredient
 
-
+#Тестовый комментарий для проверки релиза
 class IngredientFilter(rest_filters.FilterSet):
     query = CharFilter(field_name='title', lookup_expr='icontains')
 
