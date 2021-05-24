89b4e1c524e34f8cd04a0363eecc156499431f53
Boris Sokolov <tjsonyk@gmail.com>
2021-05-24 23:38:06 +0300

    recipe_view bugfix

--- recipes/views.py
+++ recipes/views.py
@@ -123,7 +123,7 @@ def recipe_delete(request, recipe_id):
         recipe.delete()
     return redirect('main-page')
 
-
+#comment fo rdockerhub
 def recipe_view(request, username, reсipe_id):
     recipe = get_object_or_404(
         Recipe,
