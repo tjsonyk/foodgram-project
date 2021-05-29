b90be709cbb17c41f03e8d3ab34b542e2746c377
Boris Sokolov <tjsonyk@gmail.com>
2021-05-29 22:40:29 +0300

    mb last bugfix

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.shortcuts import get_object_or_404
 
-from .models import Recipe, Amount
+from .models import Recipe, Amount, Ingredient
 
 
 class RecipeForm(forms.ModelForm):
