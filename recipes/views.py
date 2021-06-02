9c8c1ef8db0b08e34b8cbd12661c53cafc94a408
Boris Sokolov <tjsonyk@gmail.com>
2021-06-02 17:39:17 +0300

    bugfix with recipe_view

--- recipes/views.py
+++ recipes/views.py
@@ -98,7 +98,7 @@ def recipe_view(request, username, recipe_id):
         author__username=username,
         id=recipe_id
     )
-    user = get_object_or_404(User, user=username)
+    user = get_object_or_404(User, username=username)
     return render(request, 'singlePage.html', {'recipe': recipe})
 
 
