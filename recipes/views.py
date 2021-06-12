df3c1c114e6a6802d8e8947940afbded562e616d
Boris Sokolov <tjsonyk@gmail.com>
2021-06-12 15:28:11 +0300

    test

--- recipes/views.py
+++ recipes/views.py
@@ -52,6 +52,9 @@ def new_recipe(request):
         files=request.FILES or None
     )
 
+    if not ingredients:
+        form.add_error(None, 'Добавьте ингредиенты')
+
     if form.is_valid():
         if not ingredients:
             form.add_error(None, 'Добавьте ингредиенты')
