dccf9e79df5d827cc02cc5a520b580040bc0108a
Boris Sokolov <tjsonyk@gmail.com>
2021-06-12 11:35:51 +0300

    auth url added

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -106,7 +106,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_URL = '/auth/login/'
+LOGIN_URL = '/personal/auth/'
 LOGIN_REDIRECT_URL = 'main-page'
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
