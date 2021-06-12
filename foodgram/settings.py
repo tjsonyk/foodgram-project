282bdb8dca2ce013ff725662bdef9a984b7e6228
Boris Sokolov <tjsonyk@gmail.com>
2021-06-12 11:46:26 +0300

    settings login_url edited

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -106,7 +106,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_URL = '/personal/auth/'
+LOGIN_URL = '/personal/auth/login/'
 LOGIN_REDIRECT_URL = 'main-page'
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
